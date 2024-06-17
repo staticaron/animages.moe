76158218d8d7991c21dd649d041677d50ba7bda7
Devanshu Mishra <devm19102003@gmail.com>
2024-06-18 00:06:43 +0530

    <add><random image on every reload>

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import pdb
 from flask import Flask, send_file
+import os
 import random
 
 app = Flask("animages.moe")
@@ -7,18 +9,18 @@ app = Flask("animages.moe")
 def home():
     return "<h1>animages.moe</h1>"
 
-@app.route("/main")
-def main_page():
-    return "Main Page"
-
-@app.route("/main/main.png", methods=['GET'])
-def send_main():
-    r_value = random.randint(0, 1)
+@app.route("/<username>/<collection>.png", methods=['GET'])
+def send_main(username, collection):
     
-    if r_value == 0:
-        return send_file("images/main/art1.png", download_name="file.png", mimetype="image/png")
-    else:
-        return send_file("images/main/art2.png", download_name="file.png", mimetype="image/png")
+    path = f"images/{username}/{collection}/"
+    
+    files = os.listdir(path)
+    
+    r_value = random.randint(0, len(files) - 1)
+    
+    selected_file_path = path + files[r_value]
+    
+    return send_file(selected_file_path, mimetype="image/png")
 
 if __name__ == "__main__":
     app.run(debug=True)
